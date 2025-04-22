42c7460f6b0938457e7dfd7ceef8f1f4a61ad47b
suleyman pehlivan <stepcups@icloud.com>
2025-04-22 23:15:36 +0300

    TypeScript bağımlılıkları kaldırıldı

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path: false }
+    return config
   }
 }
 
